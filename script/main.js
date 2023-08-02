2da4556bb1bf6ac35d3e2def7aa7b58ddca6cbae
Kristen-Klinkert <125909147+Kristen-Klinkert@users.noreply.github.com>
2023-08-02 17:45:01 +0200

    MidTerm Assesment
    final resalt of final assesment

---
+++ script/main.js
@@ -0,0 +1,27 @@
+$("#rowOne").click(function(){
+
+    console.log("click")
+
+    $(".row-one").remove()
+})
+
+$("#rowTwo").click(function(){
+
+    console.log("click")
+
+    $(".row-two").remove()
+})
+
+$("#rowThree").click(function(){
+
+    console.log("click")
+
+    $(".row-three").remove()
+})
+
+$("#rowFour").click(function(){
+
+    console.log("click")
+
+    $(".row-four").remove()
+})
